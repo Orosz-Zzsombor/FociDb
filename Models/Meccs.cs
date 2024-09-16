225060ee5f45e64b5ed784a48de142ecd30884e3
orosz.zsombor.oliver <orosz.zsombor.oliver@BURG-1E03-06>
2024-09-16 12:58:03 +0200

    Add project files.

---
+++ Models/Meccs.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Meccs
+    {
+        public int id { get; set;}
+        public int fordulo { get; set; }
+        public int haziveg { get; set; }
+        public int vendegveg { get; set; }
+        public int hazaifelido { get; set; }
+        public int vendegfelido { get; set; }
+        public string? hazainev { get; set; }
+        public string? vendegnev { get; set; }
+    }
+}
